110c163ea290b59d80c2362dc216e71790662974
prodhanm <mustafizur.prodhan@gmail.com>
2022-03-24 22:39:22 -0400

    created the function that results in overlaping spiral of an octagon.

--- overlap.py
+++ overlap.py
@@ -2,3 +2,26 @@ import turtle
 import tkinter
 from random import randint
 
+turtle.bgcolor("black")
+
+turtle.speed(0)
+turtle.pensize(2)
+
+def spiral_shape():
+    side = 5
+    for i in range(180):
+        for j in range(180):
+            r = randint(0, 255)
+            g = randint(0, 255)
+            b = randint(0, 255)
+
+            turtle.colormode(255)
+            turtle.color(r, g, b)
+
+            turtle.forward(side)
+            turtle.left(45)
+
+            side += 2
+    return side
+
+spiral_shape()
